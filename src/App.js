5e8ea1c37e5a8333d39cea4225edc23826b9c05f
Kwabena <kwabenao2k@gmail.com>
2022-08-24 20:55:53 -0400

    updated styling

--- src/App.js
+++ src/App.js
@@ -1,190 +1,18 @@
-<<<<<<< HEAD
-import axios from "axios";
-import "./App.css";
-import React, { useEffect, useState } from "react";
-import AddShow from "./components/Addshow";
-import { unstable_renderSubtreeIntoContainer } from "react-dom";
-
-function App() {
-  const [ticket, setTicket] = useState({
-    name: "",
-    id: "",
-    priceRanges: [],
-  });
-
-  const [data, setData] = useState([]);
-
-  //userinput for keyword
-  const [keyWord, setKeyWord] = useState("");
-
-  //id on the selected event
-  const [id, setId] = useState("");
-
-  const [track, setTracker] = useState(0);
-
-  const [size, setSize] = useState(0);
-
-  const [show, setShow] = useState(false);
-
-  const key = `0TsZKUciU5HKm4ylnIBkwVoD8U4aPAgY`;
-
-  const getAnswer = async () => {
-    await axios({
-      url: `https://app.ticketmaster.com/discovery/v2/events`,
-      method: "GET",
-      dataResponse: "json",
-      params: {
-        format: "json",
-        apikey: key,
-        keyword: keyWord,
-        size: 10,
-      },
-    })
-      .then((response) => {
-        const dataTest = response.data._embedded.events;
-        setData(dataTest);
-      })
-      .catch((error) => {
-        alert(error.message);
-      });
-  };
-
-  useEffect(() => {
-    axios({
-      url: `https://app.ticketmaster.com/discovery/v2/events/${id}`,
-      method: "GET",
-      dataResponse: "json",
-      params: {
-        format: "json",
-        apikey: key,
-      },
-    }).then((response) => {
-      setTicket({
-        name: response.data.name,
-        id: response.data.id,
-        date: response.data.date,
-      });
-    });
-  }, [id]);
-
-  const getMoreInfo = () => {
-    return (
-      <div key={ticket.id}>
-        <p>{ticket.id}</p>
-      </div>
-    );
-  };
-
-  const renderInfo = () => {
-    return data.map((data) => {
-      return (
-        <div key={data.id}>
-          <div>
-            <img src={data.images[0].url} alt={data.name} />
-            <p>{data.name}</p>
-            <p>{data._embedded.venues[0].name}</p>
-            <p>{data._embedded.venues[0].city.name}</p>
-            <p>{data._embedded.venues[0].address.line1}</p>
-            <p>{data._embedded.venues[0].url}</p>
-            <p>{data.dates.start.localDate}</p>
-            <p>
-              {new Date(data.dates.start.dateTime).toLocaleTimeString("en-IN", {
-                hour: "2-digit",
-                minute: "2-digit",
-              })}
-            </p>
-            <p>{data.dates.timezone}</p>
-            {data.priceRanges === undefined ? (
-              <div>
-                <p>Price Range not available</p>
-              </div>
-            ) : (
-              <div>
-                <p>{data.priceRanges[0].currency}</p>
-                <p>{Math.round(data.priceRanges[0].min)}</p>
-                <p>{Math.round(data.priceRanges[0].max)}</p>
-              </div>
-            )}
-            <button
-              onClick={() => {
-                setId(data.id);
-              }}
-            >
-              More info
-            </button>
-            <AddShow ticket={ticket} />
-          </div>
-        </div>
-      );
-    });
-  };
-
-  return (
-    <div className="App">
-      <input
-        value={keyWord}
-        placeholder="insert keyword"
-        type="text"
-        onChange={(e) => {
-          setKeyWord(e.target.value);
-          // console.log(data);
-        }}
-      ></input>
-      <button
-        onClick={(e) => {
-          e.preventDefault();
-          setTracker((prevCount) => prevCount + 1);
-          setShow(true);
-          getAnswer();
-          console.log(data);
-          // setSize(10)
-          // {data.map((data) => {
-          // console.log(data.name)
-          // return (
-          //   <><p key={data.id}>{data.name}</p><p>{data.id}</p><button onClick={() => {
-          //     setId(data.id);
-          //   } }>More info</button><AddShow ticket={ticket} /></>
-          // )
-
-          // })}
-        }}
-      >
-        search
-      </button>
-      {show ? renderInfo() : <React.Fragment />}
-      {/* {data.map((data) => {
-            return (
-              <>
-              <p key={data.id}>{data.name}</p>
-              <p>{data.id}</p>
-              <button onClick={() => {
-                setId(data.id)
-                }}>More info</button>
-              <AddShow ticket={ticket}/>
-              </>
-            )
-        })} */}
-=======
 import "./App.scss";
 import Header from "./components/Header";
 import SearchResults from "./components/SearchResults";
 
 function App() {
-  return(
+  return (
     <div>
       <Header />
       <SearchResults />
->>>>>>> 502e87041e7ab1518de53ecf4c7576b90374dc26
     </div>
-  )
-
+  );
 }
 
-
-<<<<<<< HEAD
 export default App;
 
-<<<<<<< HEAD
 // onclick save data.id of event into ID state
 // run ID through API
 // save API (will be an oject) into ticket state
@@ -210,8 +38,3 @@ export default App;
 // Add a chart to show cost trends across multiple lists
 // Pagination for search results
 // Allow for the private list to be authenticated through google
-=======
->>>>>>> 502e87041e7ab1518de53ecf4c7576b90374dc26
-=======
-export default App;
->>>>>>> e82aeb2257db5e25e5a954a5b14984af462a401e
--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,11 +1,11 @@
 // import "./App.scss";
 import axios from "axios";
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import AddShow from "./AddShow";
 import { unstable_renderSubtreeIntoContainer } from "react-dom";
 
 const SearchResults = () => {
- const [ticket, setTicket] = useState({
+  const [ticket, setTicket] = useState({
     name: "",
     id: "",
     max: "",
@@ -14,28 +14,28 @@ const SearchResults = () => {
     time: "",
     timezone: "",
     address: "",
-    url: ""
-  })
+    url: "",
+  });
   const [data, setData] = useState([]);
 
-  const [keyWord, setKeyWord] = useState('');
+  const [keyWord, setKeyWord] = useState("");
 
-  const [id, setId] = useState('');
+  const [id, setId] = useState("");
 
   const [track, setTracker] = useState(0);
 
   const [show, setShow] = useState(false);
 
-  const [moreInfo, setMoreInfo] = useState(false)
+  const [moreInfo, setMoreInfo] = useState(false);
 
   const [name, setName] = useState("");
 
   const [budget, setBudget] = useState(0);
 
-    //api key
+  //api key
   const key = `0TsZKUciU5HKm4ylnIBkwVoD8U4aPAgY`;
- 
-    //asynchronous axios call with an await to get our api call when we want
+
+  //asynchronous axios call with an await to get our api call when we want
   const getAnswer = async () => {
     await axios({
       url: `https://app.ticketmaster.com/discovery/v2/events`,
@@ -45,18 +45,20 @@ const SearchResults = () => {
         format: "json",
         apikey: key,
         keyword: keyWord,
-        size: 10
+        size: 10,
       },
-    }).then((response) => {
-      console.log(response)
-      const dataTest = response.data._embedded.events;
-      setData(dataTest);
-    }).catch((error) => {
-      alert(error.message)
-    });
-  } 
-  
-    //use effect axios call to store our api in the id state
+    })
+      .then((response) => {
+        console.log(response);
+        const dataTest = response.data._embedded.events;
+        setData(dataTest);
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
+  };
+
+  //use effect axios call to store our api in the id state
   useEffect(() => {
     axios({
       url: `https://app.ticketmaster.com/discovery/v2/events/${id}`,
@@ -72,152 +74,148 @@ const SearchResults = () => {
       setTicket({
         name: response.data.name,
         id: response.data.id,
-        time: (
-            id === ""
-              ?
-              'n/a'
-              :
-              (
-                new Date(response.data.dates.start.dateTime).toLocaleTimeString("en-IN", {
-                hour: "2-digit",
-                minute: "2-digit",
-              })
-              )
-              ) 
-          
-          ,
-        timezone: (
+        time:
           id === ""
-          ? 
-          'n/a'
-          :
-          (response.data.dates.timezone)
-          ),
-        address: (
-          id === ""
-          ?
-          'n/a'
-          :
-          (response.data._embedded.venues[0].address.line1)
-          ),
-        url: (
-          id === ""
-          ?
-          'n/a'
-          :
-          (response.data._embedded.venues[0].url)
-          ),
-        max: (             
-               response.data.priceRanges === undefined
-            ? 
-             'n/a'
-            :
-                (response.data.priceRanges[0].max)
-        ),
-        min: (             
-               response.data.priceRanges === undefined
-            ? 
-             'n/a'
-            :
-                (response.data.priceRanges[0].min)
-        ),
-        venue: (
-          id === ""
-          ?
-          'n/a'
-          :
-          (response.data._embedded.venues[0].city.name)
-          )
-      })
-    })
+            ? "n/a"
+            : new Date(response.data.dates.start.dateTime).toLocaleTimeString(
+                "en-IN",
+                {
+                  hour: "2-digit",
+                  minute: "2-digit",
+                }
+              ),
+
+        timezone: id === "" ? "n/a" : response.data.dates.timezone,
+        address:
+          id === "" ? "n/a" : response.data._embedded.venues[0].address.line1,
+        url: id === "" ? "n/a" : response.data._embedded.venues[0].url,
+        max:
+          response.data.priceRanges === undefined
+            ? "n/a"
+            : response.data.priceRanges[0].max,
+        min:
+          response.data.priceRanges === undefined
+            ? "n/a"
+            : response.data.priceRanges[0].min,
+        venue: id === "" ? "n/a" : response.data._embedded.venues[0].city.name,
+      });
+    });
   }, [id]);
-  
-  //Named function to be called on an event that will display more information on out api call   
+
+  //Named function to be called on an event that will display more information on out api call
   const getMoreInfo = () => {
-    return(
+    return (
       // <div>
       <div key={ticket.name} className="borderStyle">
-            <p>{ticket.venue}</p>
-            <p>{ticket.address}</p>
-            <p>{ticket.timezone}</p>
-            <p>{ticket.time}</p>
+        <p>{ticket.venue}</p>
+        <p>{ticket.address}</p>
+        <p>{ticket.timezone}</p>
+        <p>{ticket.time}</p>
 
-            {
-              ticket.url === undefined
-              ?
-              <p>link not available</p>
-              :
-              <a href={ticket.url} target="_blank" rel="noreferrer">Ticket</a>
-            }
+        {ticket.url === undefined ? (
+          <p>link not available</p>
+        ) : (
+          <a href={ticket.url} target="_blank" rel="noreferrer">
+            Ticket
+          </a>
+        )}
+      </div>
+      // </div>
+    );
+  };
+  // new Date(response.data.dates.start.dateTime).toLocaleTimeString("en-IN", {
+  //   hour: "2-digit",
+  //   minute: "2-digit",
+  // });
 
-          </div>
-        // </div>
-      )
-  }
-
-    //Named function that will map over our api array and return the relevant search results for our user.
+  //Named function that will map over our api array and return the relevant search results for our user.
   const renderInfo = () => {
     return data.map((data) => {
       return (
         <div key={data.id}>
-          <div>
-            <img src={data.images[0].url} alt={data.name}/>
-            <p>{data.name}</p>
-            <p>{data._embedded.venues[0].name}</p>
-            <p>{data.dates.start.localDate}</p>
-            {/* conditionally rendoring our call to the getmoreinfo function   */}
+          <div className="contentInfo">
+            <img src={data.images[0].url} alt={data.name} />
+            <div className="infoContainer">
+              <h2>{data.name}</h2>
+              <h3>Venue: {data._embedded.venues[0].name}</h3>
+              <p>{data.dates.start.localDate}</p>
 
-            {/* error handle to due to some api calls not containing a price range object */}
-            {             
-               data.priceRanges === undefined
-               ? 
-               <div>
-                <p>Price Range not available</p>
-              </div>
-            :
-            <div>
-                <p>{data.priceRanges[0].currency}</p>
-                <p>{Math.round(data.priceRanges[0].min)}</p>
-                <p>{Math.round(data.priceRanges[0].max)}</p>
-              </div> 
-              
-            }        
-            {data.id === ticket.id && getMoreInfo()}
-          {/* event listener on our button to send the corresponding id stored in state as well as changing our moreInfo state to true */}
-            <button onClick={() => {
-              setMoreInfo(true);
-              setId(data.id)
-              console.log(ticket.url)
-              // console.log(ticket.max)
-              // console.log(ticket.min)
+              {/* conditionally rendoring our call to the getmoreinfo function   */}
 
-              }}
-              >More info
-            </button>
-            {/* passing our addshow component that will handle the removal of adding user selected show into firebase */}
-            <AddShow ticket={ticket} name={name} budget={budget}/>
+              {/* error handle to due to some api calls not containing a price range object */}
+              {data.priceRanges === undefined ? (
+                <div>
+                  <p>Price Range not available</p>
+                </div>
+              ) : (
+                <div>
+                  <p>Currency: {data.priceRanges[0].currency}</p>
+                  <p>Minimum price: ${Math.round(data.priceRanges[0].min)}</p>
+                  <p>Maximum price: ${Math.round(data.priceRanges[0].max)}</p>
+                </div>
+              )}
+            </div>
+            <div className="getMoreInfo">
+              {data.id === ticket.id && getMoreInfo()}
+              {/* event listener on our button to send the corresponding id stored in state as well as changing our moreInfo state to true */}
+              <button
+                onClick={() => {
+                  setMoreInfo(true);
+                  setId(data.id);
+                  console.log(ticket.url);
+                }}
+              >
+                More info
+              </button>
+
+              {/* passing our addshow component that will handle the removal of adding user selected show into firebase */}
+              <AddShow ticket={ticket} name={name} budget={budget} />
+            </div>
           </div>
         </div>
-      )
-  })}
+      );
+    });
+  };
 
-    //returning our rendered info named component, calling our getanswer function with our stored promised axios call
+  //returning our rendered info named component, calling our getanswer function with our stored promised axios call
   return (
     <div className="App wrapper">
-        <input placeholder="insert list name" type="text" onChange={(e) => { setName(e.target.value)}}></input>
-        <input placeholder="insert budget" type="number" onChange ={(e) => { setBudget(e.target.value)}}></input>
-        <input value={keyWord} placeholder="Search for an Event" type="text" onChange={(e) => {
-          setKeyWord(e.target.value)
-        }} ></input>
-        <button onClick={(e) => {
-          e.preventDefault()
-          setTracker(prevCount => prevCount +1);
+      <input
+        placeholder="Enter a list name"
+        type="text"
+        onChange={(e) => {
+          setName(e.target.value);
+        }}
+      ></input>
+      <input
+        placeholder="Enter a budget"
+        type="number"
+        onChange={(e) => {
+          setBudget(e.target.value);
+        }}
+      ></input>
+      <input
+        value={keyWord}
+        placeholder="Search for an Event"
+        type="text"
+        onChange={(e) => {
+          setKeyWord(e.target.value);
+        }}
+      ></input>
+      <button
+        onClick={(e) => {
+          e.preventDefault();
+          setTracker((prevCount) => prevCount + 1);
           setShow(true);
           getAnswer();
-        }}>search</button>
-        {show ? renderInfo() : <React.Fragment />}
+        }}
+        className="searchBtn"
+      >
+        search
+      </button>
+      {show ? renderInfo() : <React.Fragment />}
     </div>
   );
-}
+};
 
 export default SearchResults;

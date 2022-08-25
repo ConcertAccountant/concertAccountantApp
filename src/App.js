f5df88a0257f4b68a4d3f20019f8411fc143bc24
Kwabena <kwabenao2k@gmail.com>
2022-08-25 15:25:24 -0400

    further changes to styling

--- src/App.js
+++ src/App.js
@@ -1,50 +1,18 @@
 import "./App.scss";
 import Header from "./components/Header";
 import SearchResults from "./components/SearchResults";
-import { FaBeer } from 'react-icons/fa';
-
-
+import Login from "./components/Login";
+import { FaBeer } from "react-icons/fa";
 
 function App() {
   return (
     <div>
       <Header />
+
       <SearchResults />
+      <Login />
     </div>
-<<<<<<< HEAD
   );
 }
 
 export default App;
-
-// onclick save data.id of event into ID state
-// run ID through API
-// save API (will be an oject) into ticket state
-// display info on screen
-
-// PSUEDOCODE
-
-//initialize firebase
-// Create state items to hold data from the API and user input X
-// Create a component to display the information X
-// Create a component to hold userinput using "getDatabase" method X
-// Create a function to make the API call with the correct search parameters X
-// Userinput is linked to API call where user selects which items get added to the list X
-// This data is then sent to firebase X
-// Display the list the user makes as a public list O
-// Complete published lists page broken down by price O
-// Create a function to handle the remove, getDatabase and reference. getDatabase and reference would allow for the user to delete items from the list X firebase O display on page
-// Error handling where if the user enters an invalid concert/show an alert pops up and if the user enters a negative budget O
-
-// STRETCH GOALS:
-// Profanity API to allow appropiate names for the list
-// Sorting and filtering search results
-// Add a chart to show cost trends across multiple lists
-// Pagination for search results
-// Allow for the private list to be authenticated through google
-=======
-  )
-}
-
-export default App;
->>>>>>> eff80333bbd864d1e65762604191baea8ef86bec
--- src/components/Addshow.js
+++ src/components/Addshow.js
@@ -1,59 +1,64 @@
 import { useState } from "react";
-import { getDatabase, push, ref, remove, set } from 'firebase/database';
-import firebase from './Firebase';
-
+import { getDatabase, push, ref, remove, set } from "firebase/database";
+import { firebase } from "./Firebase";
 
 const AddShow = (props) => {
+  const [userInput, setUserInput] = useState("");
 
-    const [userInput, setUserInput] = useState("");
+  const handleInputChange = (e) => {
+    setUserInput(e.target.value);
+  };
 
-    const handleInputChange = (e) => {
-        setUserInput(e.target.value)
-    }
+  const myList = () => {
+    // Create a new post reference with an auto-generated id
+    const db = getDatabase();
+    const postListRef = ref(db, props.name);
+    // console.log(postListRef._path.pieces_[0])
+    const newPostRef = push(postListRef);
+    set(newPostRef, {
+      name: props.ticket.name,
+      budget: props.budget,
+      min: props.ticket.max,
+      max: props.ticket.min,
+    });
+  };
 
-    const myList = () => {
-        // Create a new post reference with an auto-generated id
-        const db = getDatabase();
-        const postListRef = ref(db, props.name);
-        // console.log(postListRef._path.pieces_[0])
-        const newPostRef = push(postListRef);
-        set(newPostRef, {
-            name: props.ticket.name,
-            budget: props.budget,
-            min: props.ticket.max,
-            max: props.ticket.min
-        });
-    }
+  // const handleSubmit = () => {
+  //     const database = getDatabase(firebase);
+  //     const dbRef = ref(database);
+  //     push(dbRef, props.ticket);
+  //     // setUserInput("");
+  // }
 
-    // const handleSubmit = () => {
-    //     const database = getDatabase(firebase);
-    //     const dbRef = ref(database);
-    //     push(dbRef, props.ticket);
-    //     // setUserInput("");
-    // }
+  const handleRemove = () => {
+    const database = getDatabase(firebase);
+    const dbRef = ref(database);
+    remove(dbRef, props.ticket);
+  };
 
-    const handleRemove = () => {
-        const database = getDatabase(firebase)
-        const dbRef = ref(database)
-        remove(dbRef, props.ticket)
-    }
-
-    return (
-        <section>
-            <div className="wrapper">
-                <button onClick={(e) => {
-                e.preventDefault();
-                // handleSubmit();
-                myList();
-                }}>Save</button>
-                <button onClick={(e) => {
-                e.preventDefault();
-                handleRemove();
-                }}
-                >remove</button>
-            </div>
-        </section>
-    )
-}
+  return (
+    <section>
+      <div className="wrapper">
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            // handleSubmit();
+            myList();
+          }}
+        >
+          Save
+        </button>
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            handleRemove();
+          }}
+        >
+          remove
+        </button>
+      </div>
+    </section>
+  );
+};
 
 export default AddShow;
